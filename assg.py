7d2d7b3ee7b42ed24e8fb0426c3f25aa70ca0a27
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2022-05-02 13:54:25 -0400

    Add files via upload

---
+++ assg.py
@@ -0,0 +1,37 @@
+import csv
+
+new_content = []
+
+with open("employees.csv") as csvfile:
+    file_content = csv.reader(csvfile)
+    file_list = list(file_content)[1:]
+    maxs = 0
+  
+    for i in range(len(file_list)):
+        for row in file_list:
+            if row not in new_content:
+                salary = int(row[3])
+                if salary>=maxs:
+                    
+                    max = salary
+                    max_employee = row
+                    
+        
+        new_content.append(max_employee)
+        maxs = 0
+                    
+    
+
+for i in new_content:
+    print(i)       
+        
+        
+
+
+
+        
+        
+
+
+
+
